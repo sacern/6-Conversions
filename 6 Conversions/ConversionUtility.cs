cb0960d8755c1df0be686fa9cb9a90bd910d9950
Steve <Steve@LAPTOP-CS7JJ3LE>
2016-10-14 19:41:14 -0400

    Add project files.

---
+++ 6 Conversions/ConversionUtility.cs
@@ -0,0 +1,75 @@
+﻿namespace _6_Conversions
+{
+internal class ConversionUtility
+{
+    public const double KM_PER_MILE = 1.60934;
+    public const int INCHES_PER_FEET = 12;
+    public const int FEET_PER_MILE = 5280;
+    public const int SECONDS_PER_MINUTE = 60;
+    public const int HOURS_PER_DAY = 24;
+    public const int CENTS_PER_DOLLAR = 100;
+
+    public ConversionUtility()
+    {
+
+    }
+
+
+
+//Converts miles to kilometers
+public double convertMileToKM(float mile)
+{
+    double result = 0;
+    result = mile * KM_PER_MILE;
+    return result;
+}
+
+//Converts feet to inches
+public double convertFeetToInches(float feet)
+{
+    double result = 0;
+    result = feet * INCHES_PER_FEET;
+    return result;
+
+}
+
+//Converts Miles to feet
+public double convertMilesToFeet(float mile)
+{
+    double result = 0;
+    result = mile * FEET_PER_MILE;
+    return result;
+
+}
+
+//Converts minutes to seconds
+public double convertMinutesToSeconds(float minutes)
+{
+    double result = 0;
+    result = minutes * SECONDS_PER_MINUTE;
+    return result;
+
+}
+
+//Converts days to hours
+public double convertHoursPerDay(float days)
+{
+    double result = 0;
+    result =days * HOURS_PER_DAY;
+    return result;
+
+}
+
+//Converts dollars to cents
+public double convertCentsToDollars(float cent)
+{
+    double result = 0;
+    result = cent / CENTS_PER_DOLLAR;
+    return result;
+
+}
+
+
+}
+}
+    
---
+++ 6 Conversions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //converts miles to kilometers
+            ConversionUtility cov = new ConversionUtility();
+            double km = cov.convertMileToKM(400);
+            Console.Write("400 miles is equal to ");
+            Console.Write("{0:n2}", km);
+            Console.WriteLine(" Kilometers");
+
+            //Converts feet to inches
+            double inch = cov.convertFeetToInches(400);
+            Console.Write("400 feet is equal to ");
+            Console.WriteLine(inch + " Inches");
+
+            //converts days to hours
+            double hour = cov.convertHoursPerDay(400);
+            Console.Write("400 days is equal to ");
+            Console.WriteLine(hour + " Hours");
+
+             //converts miles to feet
+            double ft = cov.convertMilesToFeet(400);
+            Console.Write("400 miles is equal to ");
+            Console.Write("{0:N3}", ft);
+            Console.WriteLine(" Feet");
+
+            
+             //converts cents to dollars
+            double dollar = cov.convertCentsToDollars(400);
+            Console.Write("400 cents is equal to ");
+            Console.WriteLine("{0:C2}", dollar);
+            
+
+             //converts minutes to seconds
+            double sec = cov.convertMinutesToSeconds(400);
+            Console.Write("400 minutes is equal to ");
+            Console.WriteLine(sec + " seconds");
+
+            Console.ReadLine();
+        }
+    }
+}
